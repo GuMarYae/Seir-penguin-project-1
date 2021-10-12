2762f35e8f5a732e72bad25f7d04913c6399e13d
GuMarYae <tddanielsjr@gmail.com>
2021-10-11 22:16:04 -0400

    On to JS

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+/* ******************************************
+************* App State ********************
+****************************************** */
+
+
+
+
+
 // const URL = "https://cdn.contentful.com/spaces/vcds7w56i708/environments/master/entries?access_token=pGzEHrJ-qIruLLoaxmXNvlHhfYn8k1VclHPq-ylZePI&content_type=triviaQ"
 //       $.ajax(URL).then((data) => {
 //         console.log(data)
