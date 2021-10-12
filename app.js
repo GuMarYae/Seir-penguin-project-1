c2ff54524292b1ad0cc0966102c02f87b60d8588
GuMarYae <tddanielsjr@gmail.com>
2021-10-11 23:22:54 -0400

    map function to make a new array with just the questions

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 /* ******************************************
-************* App State ********************
-****************************************** */
+ ************* App State ********************
+ ****************************************** */
 
+const state = {
+  player1: 0,
+  player2: 0,
+  currentQuestion: {},
+};
 
+/* ******************************************
+ ************* DOM Element *******************
+ *********************************************/
 
+const $questions = $("#questions");
+// const $answer = $("#answer")
+const $a = $("#a");
+const $b = $("#b");
+const $c = $("#c");
+const $d = $("#d");
 
+const $player1Score = $("#player1 h4");
+const $player2Score = $("#player2 h4");
 
-// const URL = "https://cdn.contentful.com/spaces/vcds7w56i708/environments/master/entries?access_token=pGzEHrJ-qIruLLoaxmXNvlHhfYn8k1VclHPq-ylZePI&content_type=triviaQ"
-//       $.ajax(URL).then((data) => {
-//         console.log(data)
-//       })
+/* ******************************************
+ ************* functions *******************
+ *********************************************/
+
+// console.log($d)
+
+const URL =
+  "https://cdn.contentful.com/spaces/vcds7w56i708/environments/master/entries?access_token=pGzEHrJ-qIruLLoaxmXNvlHhfYn8k1VclHPq-ylZePI&content_type=triviaQ";
+$.ajax(URL).then((data) => {
+  
+  questions = data.items.map((q) => q.fields);  //the map function
+     console.log(data);
+     console.log(questions);
+});
+
+let questions = [];
