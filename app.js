037bbce9256dbccd755eef1c0e292d213c115364
GuMarYae <tddanielsjr@gmail.com>
2021-10-11 20:35:27 -0400

    made plain text layout pt1

---
+++ app.js
@@ -0,0 +1,4 @@
+// const URL = "https://cdn.contentful.com/spaces/vcds7w56i708/environments/master/entries?access_token=pGzEHrJ-qIruLLoaxmXNvlHhfYn8k1VclHPq-ylZePI&content_type=triviaQ"
+//       $.ajax(URL).then((data) => {
+//         console.log(data)
+//       })
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Project 1 Documentation
-## by Tony Daniels
+## By Tony Daniels
 
 
 ## Introduction
